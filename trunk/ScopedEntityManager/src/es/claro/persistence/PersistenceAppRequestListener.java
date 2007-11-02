87f91280cc07c7c5aa5820816f005eaa6f28cd8f
granraul <granraul@b137e223-3e36-0410-8ac1-5bf56fdaf921>
2007-11-02 11:27:11 +0000

    Upgraded listener behaviour: configurable persistence unit name, automatic closing of EMF (previously was client responsibility)

--- trunk/ScopedEntityManager/src/es/claro/persistence/PersistenceAppRequestListener.java
+++ trunk/ScopedEntityManager/src/es/claro/persistence/PersistenceAppRequestListener.java
@@ -1,5 +1,7 @@
 package es.claro.persistence;
 
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 import javax.servlet.ServletRequestListener;
 import javax.servlet.ServletRequestEvent;
 
@@ -10,14 +12,39 @@ import javax.servlet.ServletRequestEvent;
  * Web application lifecycle listener.
  */
 
-public class PersistenceAppRequestListener implements ServletRequestListener {
+public class PersistenceAppRequestListener
+implements ServletContextListener, ServletRequestListener {
+  
+  private static final String PU_PARAMETER_NAME =
+      "es.claro.persistence.PERSISTENCE_UNIT";
+  
+  public void contextInitialized(ServletContextEvent evt) {
+
+    PersistenceManager pm = PersistenceManager.getInstance();
+    
+    if (pm instanceof ScopedPersistenceManager) {
+      
+      // If the name of the persistence unit has not been set yet
+      if (PersistenceManager.getPersistenceUnit() == null) {
+        String pu = evt.getServletContext().getInitParameter(PU_PARAMETER_NAME);
+        PersistenceManager.setPersistenceUnit((pu != null)? pu
+                               : PersistenceManager.DEFAULT_PERSISTENCE_UNIT);
+      }
+    }
+  }
+
+  public void contextDestroyed(ServletContextEvent evt) {
+    
+    PersistenceManager.getInstance().closeEntityManagerFactory();
+  }
+  
   /**
    * ### Method from ServletRequestListener ###
    * 
    * The request is about to come into scope of the web application.
    */
   public void requestInitialized(ServletRequestEvent evt) {
-    // TODO add your code here:
+   
   }
 
   /**
