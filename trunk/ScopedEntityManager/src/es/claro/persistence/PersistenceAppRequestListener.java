becbabea676cdb21c192be2455fa73a983ab76f6
granraul <granraul@b137e223-3e36-0410-8ac1-5bf56fdaf921>
2007-11-05 09:13:38 +0000

    Listener generalized.

--- trunk/ScopedEntityManager/src/es/claro/persistence/PersistenceAppRequestListener.java
+++ trunk/ScopedEntityManager/src/es/claro/persistence/PersistenceAppRequestListener.java
@@ -1,7 +1,5 @@
 package es.claro.persistence;
 
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
 import javax.servlet.ServletRequestListener;
 import javax.servlet.ServletRequestEvent;
 
@@ -13,30 +11,7 @@ import javax.servlet.ServletRequestEvent;
  */
 
 public class PersistenceAppRequestListener
-implements ServletContextListener, ServletRequestListener {
-  
-  private static final String PU_PARAMETER_NAME =
-      "es.claro.persistence.PERSISTENCE_UNIT";
-  
-  public void contextInitialized(ServletContextEvent evt) {
-
-    PersistenceManager pm = PersistenceManager.getInstance();
-    
-    if (pm instanceof ScopedPersistenceManager) {
-      
-      // If the name of the persistence unit has not been set yet
-      if (PersistenceManager.getPersistenceUnit() == null) {
-        String pu = evt.getServletContext().getInitParameter(PU_PARAMETER_NAME);
-        PersistenceManager.setPersistenceUnit((pu != null)? pu
-                               : PersistenceManager.DEFAULT_PERSISTENCE_UNIT);
-      }
-    }
-  }
-
-  public void contextDestroyed(ServletContextEvent evt) {
-    
-    PersistenceManager.getInstance().closeEntityManagerFactory();
-  }
+extends PersistenceAppListener implements ServletRequestListener {
   
   /**
    * ### Method from ServletRequestListener ###
